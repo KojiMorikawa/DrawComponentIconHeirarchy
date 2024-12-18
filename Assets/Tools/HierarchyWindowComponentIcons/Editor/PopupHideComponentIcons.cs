b81208c2bd836d95627afeac3d6436652433fde0
Koji Morikawa(private) <mksakura44@gmail.com>
2024-12-19 00:33:51 +0900

    Commit first code

---
+++ Assets/Tools/HierarchyWindowComponentIcons/Editor/PopupHideComponentIcons.cs
@@ -0,0 +1,65 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace HandMaidEditorUtility
+{
+    public class PopupHideComponentIcons : PopupWindowContent
+    {
+        private const int _iconSize = 14;
+        private const int _padding = 4;
+        private const float _maxWindowWidth = 24f * 10f;
+
+        private Component[] _components;
+        private int _startIndex;
+        private int _componentsCount;
+        private int _totalCount;
+
+        public PopupHideComponentIcons(Component[] components, int startIndex)
+        {
+            _components = components;
+            _startIndex = startIndex;
+        }
+
+        public override Vector2 GetWindowSize()
+        {
+            float componentWidth = _iconSize + _padding;
+            _componentsCount = Mathf.FloorToInt((_maxWindowWidth - _padding) / componentWidth);
+            _totalCount = Mathf.CeilToInt((_components.Length - _startIndex) / (float)_componentsCount);
+
+            float windowWidth = Mathf.Min(_maxWindowWidth, ((_components.Length - _startIndex) * componentWidth) + _padding);
+            float windowHeight = _totalCount * (_iconSize + _padding) + _padding;
+            return new Vector2(windowWidth, windowHeight);
+        }
+
+        public override void OnGUI(Rect rect)
+        {
+            int index = _startIndex;
+            for (int row = 0; row < _totalCount; row++)
+            {
+                EditorGUILayout.BeginHorizontal();
+
+                for (int col = 0; col < _componentsCount; col++)
+                {
+                    if (index >= _components.Length)
+                        break;
+
+                    GUIStyle style = new GUIStyle()
+                    {
+                        fixedWidth = _iconSize,
+                        fixedHeight = _iconSize,
+                        margin = new RectOffset(_padding, _padding, _padding, _padding)
+                    };
+
+                    Texture icon = AssetPreview.GetMiniThumbnail(_components[index]);
+                    if (GUILayout.Button(icon, style)) { }
+
+                    index++;
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+    }
+}
+#endif
---
+++ ProjectSettings/Assets/Tools/HierarchyWindowComponentIcons/Editor/HierarchyWindowComponentIcons.cs
@@ -0,0 +1,84 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+// Site URL of the referenced code
+// http://anchan828.hatenablog.jp/entry/2013/05/12/021914
+
+namespace HandMaidEditorUtility
+{
+    [InitializeOnLoad]
+    public class HierarchyWindowComponentIcons : Editor
+    {
+        private const int _hideIconSize = 14;
+        private const int _iconSize = 16;
+        private const float _padding = 6f;
+
+        static HierarchyWindowComponentIcons()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += (instanceID, rect) =>
+            {
+                GameObject obj = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+                if (obj != null)
+                {
+                    DrawComponentIcons(instanceID, rect, obj);
+                }
+            };
+        }
+        static void DrawComponentIcons(int instanceID, Rect rect, GameObject obj)
+        {
+            bool isPrefab = PrefabUtility.GetPrefabAssetType(obj) != PrefabAssetType.NotAPrefab;
+            Rect iconRect = new Rect(rect.x + rect.width, rect.y, _iconSize, rect.height);
+            int maxIconsToShow = Mathf.FloorToInt(CalculateWidthForIcons(obj.name, rect, isPrefab) / (_iconSize + _padding));
+
+            Component[] components = obj.GetComponents<Component>();
+            List<Texture> textures = new List<Texture>();
+
+            foreach (Component component in components)
+            {
+                Texture icon = AssetPreview.GetMiniThumbnail(component);
+                if (icon != null)
+                {
+                    textures.Add(AssetPreview.GetMiniThumbnail(component));
+                }
+            }
+
+            int showIcons = 0;
+            iconRect.x += _iconSize;
+
+            if (isPrefab)
+            {
+                iconRect.x -= _iconSize;
+                GUI.Button(iconRect, EditorGUIUtility.IconContent("ArrowNavigationRight"), GUIStyle.none);
+            }
+
+            foreach (Texture texture in textures)
+            {
+                if (showIcons > maxIconsToShow)
+                    break;
+
+                if (texture != null)
+                {
+                    iconRect.x -= _iconSize;
+                    GUI.DrawTexture(iconRect, texture);
+                    ++showIcons;
+                }
+            }
+
+            if (textures.Count > showIcons)
+            {
+                iconRect.x -= _iconSize;
+                if (GUI.Button(iconRect, (Texture)EditorGUIUtility.Load("Icons/hm_CollabChangesDeleted.png"), GUIStyle.none))
+                    PopupWindow.Show(iconRect, new PopupHideComponentIcons(obj.GetComponents<Component>(), showIcons));
+            }
+        }
+
+        private static int CalculateWidthForIcons(string objName, Rect selectionRect, bool isPrefab)
+        {
+            return (int)(selectionRect.width - (GUI.skin.label.CalcSize(new GUIContent(objName)).x + _padding) -
+                         (isPrefab ? _iconSize : 0) - (float)_iconSize);
+        }
+    }
+}
+#endif
